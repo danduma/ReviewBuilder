34f8b810fbc6f0fb3e02c3fa3cb063088c392870
Daniel Duma <danielduma@gmail.com>
2019-10-07 02:15:24 +0100

    Small fixes

--- search/__init__.py
+++ search/__init__.py
@@ -1,1 +1,3 @@
-from .scholar_search import GScholarSearcher
+from .base_search import MAX_RESULTS, Searcher, SearchResult
+from .google_scholar import GScholarSearcher
+from .metadata_harvest import enrichMetadata
