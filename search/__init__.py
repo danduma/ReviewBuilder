11f2912c4374ae3d4b4fc781ae238b91c4e65ff0
Daniel Duma <danielduma@gmail.com>
2019-10-10 15:03:22 +0100

    add reference to enrichAndUpdateMetadata

--- search/__init__.py
+++ search/__init__.py
@@ -1,3 +1,3 @@
 from .base_search import MAX_RESULTS, Searcher, SearchResult
 from .google_scholar import GScholarSearcher
-from .metadata_harvest import enrichMetadata
+from .metadata_harvest import enrichMetadata, enrichAndUpdateMetadata
