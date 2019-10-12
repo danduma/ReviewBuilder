6c499ba4223df8499d00c771ccd2fe3d8251d3ea
Daniel Duma <danielduma@gmail.com>
2019-10-12 23:25:02 +0100

    added loading scholarid from notes in bib file

--- gather_metadata.py
+++ gather_metadata.py
@@ -4,6 +4,22 @@ from search import enrichAndUpdateMetadata, SearchResult
 from argparse import ArgumentParser
 from db.bibtex import write_bibtex, read_bibtex_file
 
+import re
+
+
+def getSearchResultsFromBib(bib_entries, max_results):
+    results = []
+    for index, bib in enumerate(bib_entries[:max_results]):
+        res = SearchResult(index, bib, 'bibfile', {})
+        if bib.get('note'):
+            match = re.search('(\d+)\scites:\s.+?scholar\?cites\=(\d+)')
+            if match:
+                res.source = 'scholar'
+                res.extra_data['scholarid'] = match.group(2)
+                res.extra_data['citedby'] = match.group(1)
+        results.append(res)
+
+    return results
 
 
 def main(conf):
@@ -12,9 +28,8 @@ def main(conf):
     else:
         paperstore = None
 
-
     bib_entries = read_bibtex_file(conf.input)
-    results = [SearchResult(e, {}) for e in bib_entries[:conf.max]]
+    results = getSearchResultsFromBib(bib_entries, conf.max)
 
     found, missing = paperstore.matchResultsWithPapers(results)
 
@@ -22,14 +37,17 @@ def main(conf):
     successful, unsuccessful = enrichAndUpdateMetadata(papers_to_add, paperstore, conf.email)
 
     papers_existing = [res.paper for res in found]
-    # enrichAndUpdateMetadata(papers_existing)
+
+    if conf.force:
+        enrichAndUpdateMetadata(papers_existing)
 
     all_papers = papers_to_add + papers_existing
     write_bibtex(all_papers, conf.output)
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Gathers metadata, including the abstract, on a list of search results by searching on Crossref, PubMed, arXiv, Semantic Scholar and Unpaywall')
+    parser = ArgumentParser(
+        description='Gathers metadata, including the abstract, on a list of search results by searching on Crossref, PubMed, arXiv, Semantic Scholar and Unpaywall')
 
     parser.add_argument('-i', '--input', type=str,
                         help='Input Bibtex file with the previously cached search results')
@@ -41,6 +59,8 @@ if __name__ == '__main__':
                         help='Email to serve as identity to API endpoints')
     parser.add_argument('-c', '--cache', type=bool, default=True,
                         help='Use local cache for results')
+    parser.add_argument('-f', '--force', type=bool, default=False,
+                        help='Force updating metadata for cached results')
 
     conf = parser.parse_args()
 
