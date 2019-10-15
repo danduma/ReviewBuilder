7610e60466ef490cfe0f7194e31688f1198805de
Daniel Duma <danielduma@gmail.com>
2019-10-16 00:06:55 +0100

    initial commit

---
+++ db/ris.py
@@ -0,0 +1,64 @@
+from .bibtex import parseBibAuthors
+
+mapping = [
+    ('address', 'AD'),
+    ('doi', 'DO'),
+    ('eprint', 'LK'),
+    ('editor', 'ED'),
+    ('issue', 'IS'),
+    ('journal', 'JF'),
+    ('publisher', 'PB'),
+    ('title', 'TI'),
+    ('url', 'UR'),
+    ('volume', 'VL'),
+]
+
+type_mapping = {
+    'inproceedings': 'CONF',
+    'article': 'JOUR',
+    'thesis': 'THES',
+    'book': 'BOOK',
+}
+
+
+def exportBibToRIS(entries):
+    lines = []
+    for entry in entries:
+        authors = parseBibAuthors(entry['author'])
+
+        if entry['ENTRYTYPE'].lower() in type_mapping:
+            ris_type = type_mapping[entry['ENTRYTYPE'].lower()]
+        else:
+            ris_type = 'JOUR'
+
+        lines.append('TY  - ' + ris_type)
+
+        for author in authors:
+            au_line = 'AU  - %s, %s' % (author['family'], author['given'])
+            if author.get('middle'):
+                au_line += ' ' + author['middle']
+            lines.append(au_line)
+
+        # lines.append('PY - %s/%s/%s/' % (entry['year'], entry['month'], entry['day']))
+        lines.append('PY - %s///' % (entry['year'],))
+
+        pages = entry.get('pages')
+        if pages:
+            bits = pages.split('-')
+
+            lines.append('SP  - ' + bits[0])
+            lines.append('EP  - ' + bits[-1])
+
+        for eq in mapping:
+            if entry.get(eq[0]):
+                lines.append(eq[1] + '  - ' + entry[eq[0]])
+
+        lines.append('ER  - ')
+
+    return '\n'.join(lines)
+
+
+def writeBibToRISFile(entries, filename):
+    with open(filename) as f:
+        text = exportBibToRIS(entries)
+        f.write(text)
