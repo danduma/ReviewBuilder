6d2a4455e69f84e330ecf77c28c70b2e39df901d
Daniel Duma <danielduma@gmail.com>
2020-01-01 22:44:00 +0100

    adding PubMed & SemanticScholar

--- search/__init__.py
+++ search/__init__.py
@@ -1,3 +1,4 @@
 from .base_search import MAX_RESULTS, Searcher, SearchResult, getSearchResultsFromBib
 from .google_scholar import GScholarSearcher
 from .metadata_harvest import enrichMetadata, enrichAndUpdateMetadata
+from .other_search import PubMedSearcher, SemanticScholarSearcher
